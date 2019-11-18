150b4b6269b1f55f7580f6a1fbb7eb53af019dd1
Abid Hasan <abidh@hsenidmobile.com>
2019-11-18 17:27:43 +0600

    Added spring-jpa-relations starter

---
+++ src/main/kotlin/com/abid/springjparelation/SpringJpaRelationApplication.kt
@@ -0,0 +1,11 @@
+package com.abid.springjparelation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringJpaRelationApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringJpaRelationApplication>(*args)
+}
